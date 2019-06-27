54724ed7211529e020a73369b4b84f3da4e88327
Jethet <henriette.hettinga@gmail.com>
2019-06-27 16:25:55 +0200

    Create ID function for blogs

--- personalportfolio-project/blog/urls.py
+++ personalportfolio-project/blog/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.allblogs, name='allblogs'),
+    path('<int:blog_id>/', views.detail, name=detail),
 ]
+
+# The second path makes it possible to go to the individual blogs: the path
+# basically states 'look for an int after the /blog'
