81024060860bb7b7a8c0697bae01bb2ffc010c62
Jethet <henriette.hettinga@gmail.com>
2019-07-05 16:39:57 +0200

    Adjust static root

--- personalportfolio-project/personalportfolio/settings.py
+++ personalportfolio-project/personalportfolio/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
